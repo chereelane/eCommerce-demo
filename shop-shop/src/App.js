5e01e944dadc88d6fcc869499cc250902663c6b8
Cheree <38049977+chereelane@users.noreply.github.com>
2022-07-01 14:56:04 -0400

    starting homepage

--- shop-shop/src/App.js
+++ shop-shop/src/App.js
@@ -3,7 +3,44 @@ import logo from './favicon.png';
 const  App = () => {
   return (
     <div className="App">
-      Hello World
+      <div className="categories-container">
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body">
+                  <h2>Fashion</h2>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body">
+                  <h2>Architecture</h2>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body">
+                  <h2>Nature</h2>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body">
+                  <h2>Design</h2>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body">
+                  <h2>Lifestyle</h2>
+              </div>
+          </div>
+          <div className="category-container">
+              {/*<img src="" alt=""/>*/}
+              <div className="category-body">
+                  <h2>Blog</h2>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
