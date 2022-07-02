cfc786afe343bee34cd9e272984421f2dd2b2b1f
Cheree <38049977+chereelane@users.noreply.github.com>
2022-07-02 15:18:33 -0400

    created images folder and started SCSS file

--- shop-shop/src/App.js
+++ shop-shop/src/App.js
@@ -1,48 +1,53 @@
 import logo from './favicon.png';
+import './categories.styles.scss'
 
 const  App = () => {
+
+    const categories = [
+        {
+            id: 1,
+            title: 'Fashion',
+        },
+        {
+            id: 2,
+            title: 'Architecture',
+        },
+        {
+            id: 3,
+            title: 'Nature',
+        },
+        {
+            id: 4,
+            title: 'Design',
+        },
+        {
+            id: 5,
+            title: 'Blog',
+        },
+        {
+            id: 6,
+            title: 'Lifestyle',
+        },
+    ]
   return (
     <div className="App">
+        <h1 className="title"> Shop Shop</h1>
       <div className="categories-container">
-          <div className="category-container">
-              {/*<img src="" alt=""/>*/}
-              <div className="category-body">
-                  <h2>Fashion</h2>
+          {categories.map(({title}) => (
+              <div className="category-container hover">
+                  <div className="background-img">
+                      <img src="" alt=""/>
+                  </div>
+                  <div className="category-body-container">
+                      <h2>{title}</h2>
+                  </div>
               </div>
-          </div>
-          <div className="category-container">
-              {/*<img src="" alt=""/>*/}
-              <div className="category-body">
-                  <h2>Architecture</h2>
-              </div>
-          </div>
-          <div className="category-container">
-              {/*<img src="" alt=""/>*/}
-              <div className="category-body">
-                  <h2>Nature</h2>
-              </div>
-          </div>
-          <div className="category-container">
-              {/*<img src="" alt=""/>*/}
-              <div className="category-body">
-                  <h2>Design</h2>
-              </div>
-          </div>
-          <div className="category-container">
-              {/*<img src="" alt=""/>*/}
-              <div className="category-body">
-                  <h2>Lifestyle</h2>
-              </div>
-          </div>
-          <div className="category-container">
-              {/*<img src="" alt=""/>*/}
-              <div className="category-body">
-                  <h2>Blog</h2>
-              </div>
-          </div>
+              ))}
       </div>
     </div>
   );
 }
 
 export default App;
+
+
